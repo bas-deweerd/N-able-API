0c20f159c766f9506d4244d5ca7ff0ab3ff191cb
Bas de Weerd <bas-deweerd@live.nl>
2017-03-17 16:34:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,47 @@
         
         $api = new api();
         
+        $numberOfDevices =0;
+        $numberOfAvDefenders =0;
+        
+        foreach ($api->getDevices()->return as $device) {
+            $numberOfDevices++;
+            $deviceid = $device->info[0]->value;
+            foreach ($api->getDeviceStatus($deviceid)->return as $value) {
+                foreach ($value->info as $info) {
+                    //var_dump($info);
+                    //echo '<br/>';
+                    
+                    
+                    
+                    if($info->key == "devicestatus.modulename"){
+                        if($info->value == "AV Defender Status"){
+                            $numberOfAvDefenders++;
+                            
+                            
+                        }
+                    }
+                }
+            }
+        }
+        echo "Number of devices : " . $numberOfDevices . "<br />";
+        echo "Number of AV defenders: " . $numberOfAvDefenders;
+        
+  
+        
+        //echo $api->addCustomer("customername", "0622331122", "1122MM", "Kerkstraat 11", "", "Veldhoven", "NB", "NL", "Mr", "John", "Lennon", "0633221111", "31", "johnlennon@email.com", "Finance", "111");
+        
+       
+        
+        //var_dump($api->getDevices());
+        
+        
 //        foreach ($api->getDevices()->return as $value) {
 //            var_dump($value);
 //            echo '<br /><br />';
 //        }
         
-        var_dump($api->deleteCustomer(154));
+        //var_dump($api->deleteCustomer(154));
         
         //$api->getDeviceStatus("2061228974")->return
         
@@ -39,7 +74,7 @@
          
         
         //$array = $api->getCustomerListChildren("111");
-        //$api->addCustomer("customername", "0622331122", "1122MM", "Kerkstraat 11", "", "Veldhoven", "NB", "NL", "Mr", "John", "Lennon", "0633221111", "31", "johnlennon@email.com", "Finance", "111");
+
         //$api->addUser('testmail@mail.com', 'pAs_w0rdd', '111', 'John', 'Lennon', '1002');
         
         ?>
